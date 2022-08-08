376484f40868789108f8fe882f6412776f3ee35d
fabian ignacio salazar sepulveda <energyfabian12@gmail.com>
2022-08-08 17:27:29 -0400

    detalles

--- administrador/urls.py
+++ administrador/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import  path
 from .views import homeAdmin, home_empresa, tablasExtraccion, entradasExtraccion, SalidasExtraccion, OportunidadExtraccion, EntradaDiseño, salidaDiseño, oportunidadDiseño, EntradaLogistica, \
                 salidaLogistica, oportunidadLogistica, entradaCompra, salidaCompra, oportunidadesCompra, entradaUsoConsumo, salidaUsoConsumo, oportunidadUsoConsumo, \
-                entradaFin, salidaFin, oportunidadFin, tablasDiseño, tablasLogistica, tablasCompra, tablasUso, tablasFin, promedioArea 
+                entradaFin, salidaFin, oportunidadFin, tablasDiseño, tablasLogistica, tablasCompra, tablasUso, tablasFin, promedioArea, promedioHome, homeFrecuenciaDiseño, \
+                homeFrecuenciaLogistica, homeFrecuenciaCompra, homeFrecuenciaUso, homeFrecuenciaFin, frecuenciaDiseño, frecuenciaLogistica, frecuenciaCompra, frecuenciaUso, \
+                frecuenciaFin         
 
 urlpatterns = [
     path('home_admin', homeAdmin, name='home_admin'),
@@ -14,6 +16,7 @@ urlpatterns = [
     path('tablas_compra/<id>/', tablasCompra, name='tablas_compra'),
     path('tablas_uso/<id>/', tablasUso, name='tablas_uso'),
     path('tablas_fin/<id>/', tablasFin, name='tablas_fin'),
+    path('promedio_home/<id>/', promedioHome, name ='promedio_home' ),
 
     #Levenshtein
     path('promedio_area/<id>/', promedioArea, name='promedio_area'),
@@ -27,23 +30,32 @@ urlpatterns = [
     path('entradas_diseño', EntradaDiseño, name='entradas_diseño'),
     path('salida_diseño', salidaDiseño, name='salida_diseño'),
     path('oportunidad_diseño', oportunidadDiseño, name='oportunidad_diseño'),
+    path('home_frecuencia_diseño/<id>/', homeFrecuenciaDiseño, name='home_frecuencia_diseño'),
+    path('frecuencia_diseño/<id>/', frecuenciaDiseño, name='frecuencia_diseño'),
     #logistica
     path('entradas_logistica', EntradaLogistica, name='entradas_logistica'),
     path('salida_logistica', salidaLogistica, name='salida_logistica'),
     path('oportunidad_logistica', oportunidadLogistica, name='oportunidad_logistica'),
+    path('home_frecuencia_logistica/<id>/', homeFrecuenciaLogistica, name='home_frecuencia_logistica'),
+    path('frecuencia_logistica/<id>/', frecuenciaLogistica, name='frecuencia_logistica'),
     #compra
     path('entradas_compra', entradaCompra, name='entradas_compra'),
     path('salida_compra', salidaCompra, name='salida_compra'),
     path('oportunidad_compra', oportunidadesCompra, name='oportunidad_compra'),
+    path('home_frecuencia_compra/<id>/', homeFrecuenciaCompra, name='home_frecuencia_compra'),
+    path('frecuencia_compra/<id>/', frecuenciaCompra, name='frecuencia_compra'),
     #Uso consumo
     path('entradas_uso', entradaUsoConsumo, name='entradas_uso'),
     path('salidas_uso', salidaUsoConsumo, name='salidas_uso'),
     path('oportunidad_uso', oportunidadUsoConsumo, name='oportunidad_uso'),
+    path('home_frecuencia_uso/<id>/', homeFrecuenciaUso, name='home_frecuencia_uso'),
+    path('frecuencia_uso/<id>/', frecuenciaUso, name='frecuencia_uso'),
     #fin de vida
     path('entrada_fin', entradaFin, name='entrada_fin'),
     path('salidas_fin', salidaFin, name='salidas_fin'),
     path('oportunidad_fin', oportunidadFin, name='oportunidad_fin'),
-
+    path('home_frecuencia_fin/<id>/', homeFrecuenciaFin, name='home_frecuencia_fin'),
+    path('frecuencia_fin/<id>/', frecuenciaFin, name='frecuencia_fin'),
 
 
 ]
