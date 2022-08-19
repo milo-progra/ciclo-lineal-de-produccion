e6b4ce3e9423e3308e2398574827767a60c05a67
fabian ignacio salazar sepulveda <energyfabian12@gmail.com>
2022-08-19 15:02:59 -0400

    cambiando los fields en serializer.py

--- api/serializers.py
+++ api/serializers.py
@@ -5,5 +5,5 @@ class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = Usuario
         #exclude define los campos que no apareceran en la consulta
-        exclude = ['user_permissions', 'groups', 'is_active', 'date_joined', 'is_superuser']
-       
+        #exclude = ['user_permissions', 'groups', 'is_active', 'date_joined', 'is_superuser']
+        fields = 'first_name', 'last_name', 'email', 'telefono'
